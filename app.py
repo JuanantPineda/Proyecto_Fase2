878f6b080923f132b152bc87a2b58b131fec6d83
JuanantPineda <juanantpiama@gmail.com>
2024-05-15 12:47:06 +0200

    Se ha mejorado el buscador

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     listado = [15,20,25,30,35,40]
 
     if request.method == "GET":
-        valor = 10  # Fijar el valor predeterminado en 10
+        valor = 10
         url = "https://api.rawg.io/api/games"
 
         params = {
@@ -48,16 +48,38 @@ def index():
         
         return render_template("index.html",videojuegos=videojuegos,listado=listado,valor=valor)
     
-@app.route('/buscador',methods=["GET","POST"])
+@app.route('/buscador', methods=["GET", "POST"])
 def buscador():
      
+    if request.method == "GET":
+        videojuegos = []
+        return render_template("buscador.html", videojuegos=videojuegos)
+    else:
+        nombre = request.form.get("nombre")
+
+        url = "https://api.rawg.io/api/games"
+
+        params = {
+            "key": "d4641e6e548b489f919773236c54f8bb",
+            "search": nombre,
+            "page_size": 1
+        }
+
+        response = requests.get(url, params=params)
+    
+        data = response.json()
+        videojuegos = []
+
+        for var in data["results"]:
+            diccionario = {"nombre":var["name"], "imagen": var["background_image"], "año":var["released"]}
+            videojuegos.append(diccionario)
+
+        return render_template("buscador.html", videojuegos=videojuegos)
 
 
-
-
-
-     
-     return render_template("buscador.html")
+@app.route('/detalle/<nombre>')
+def detalle(nombre):
+     return render_template("detalle.html",nombre=nombre)
 
 app.run("0.0.0.0",5000,debug=True)
 
